a688e000495e4b8da4339be5bf3741ce3d205b55
Matt Cairns <opusmatt@gmail.com>
2011-09-22 22:29:02 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv=Content-type content="text/html; charset=utf-8">
+	<title>Tab</title>
+	
+	<link rel="stylesheet" href="style.css" type="text/css" media="screen" title="Site" charset="utf-8">
+	
+</head>
+<body>
+
+<div id="site-wrapper">
+	
+</div><!-- /#site-wrapper -->
+
+<!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if offline -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="_/js/libs/jquery-1.6.2.min.js"><\/script>')</script>
+
+</body>
+</html>
